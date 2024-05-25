a5c1801c3f1ff5fcbd98c1cf67adda0c0d7105a9
Tomoaki-Moriya <tomoaki.moriya1096@gmail.com>
2024-05-25 23:16:09 +0900

    add: ci/cd and testing

--- src/pdf_service.py
+++ src/pdf_service.py
@@ -1,12 +1,18 @@
 
-from weasyprint import HTML
+
+from typing import Final
 
 
 class PdfService:
 
+    def __init__(self) -> None:
+        # for testing
+        from weasyprint import HTML
+        self._HTML: Final = HTML
+
     def create_from_html(self, content: str) -> bytes:
-        pdf = HTML(string=content,
-                   encoding="utf-8").write_pdf()
+        pdf = self._HTML(string=content,
+                         encoding="utf-8").write_pdf()
         if not pdf:
             raise ValueError("PDF generation failed")
         return pdf
